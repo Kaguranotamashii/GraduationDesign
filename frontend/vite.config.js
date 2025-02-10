1d22d9896ee54248ce7a0caab5be2893d3f04bc1
宋宇然 <5592286+song_yu_ran@user.noreply.gitee.com>
2025-02-10 12:04:33 +0800

    build:为 Vite 配置添加 GitHub Pages 部署支持
    - 在 Vite 配置中添加 base 属性，设置为 '/GraduationDesign/'，以支持 GitHub Pages 部署- 优化代码格式，调整缩进和换行

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,25 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss(),
+    plugins: [
+        react(),
+        tailwindcss(),
 
-  ],
+    ],
+    base: '/GraduationDesign/', // GitHub 仓库名，确保路径正确！
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
-  server: {
-    https: {
-      key: fs.readFileSync('./key.pem'),  // 确保路径正确
-      cert: fs.readFileSync('./cert.pem') // 确保路径正确
-    },
-  }
+    server: {
+        https: {
+            key: fs.readFileSync('./key.pem'),  // 确保路径正确
+            cert: fs.readFileSync('./cert.pem') // 确保路径正确
+        },
+    }
 });
